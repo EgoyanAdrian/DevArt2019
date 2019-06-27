aec6ef2a6c9265b5c449d71459b113de97fb91ed
Spatium <44459585+Spatiu@users.noreply.github.com>
2019-06-27 12:14:23 +0200

    Fonction aletatoire

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <fonctionAleatoire.hpp>
 
 int main() {
 	std::cout << "Bonjour le mondedd !" << std::endl;
-
+	nbIP();
+	std::cout<<IP()<<std::endl;
+	std::cout<<chance(5)<<std::endl;
 	return 0;
 }
